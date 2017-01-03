b339424bbdbfe95921f627ecebc6b862a70332e2
j8761336 <105021026@live.asia.edu.tw>
2017-01-04 01:32:49 +0800

    0.0

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -6,16 +6,14 @@ public class hw03 {
 		// TODO Auto-generated method stub
 		Scanner scn =new Scanner(System.in);
 		String[] a=scn.nextLine().split(" ");
-		System.out.print(a.length);
 		for(int i=0;i<a.length-1;i++){
 			for(int j=i+1;j<=a.length-1;j++){
 				if(a[i]==a[j]){
 					a[j]=" ";
 				}
 			}
-		}for(int i=0;i<a.length;i++){
 			System.out.print(a[i]+" ");
-		}
+		}System.out.println();
 	}
 
 }
